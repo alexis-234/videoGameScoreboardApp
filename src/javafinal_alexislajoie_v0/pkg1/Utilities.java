3591437d76a184f490252df633848a258c871048
alexi <alexislajoie17@gmail.com>
2025-12-04 10:48:16 -0500

    Added a method that swaps students places basd on an array of scores 1:1 with thir scores for a given game

--- src/javafinal_alexislajoie_v0/pkg1/Utilities.java
+++ src/javafinal_alexislajoie_v0/pkg1/Utilities.java
@@ -63,6 +63,24 @@ public class Utilities {
             list[i] = currentMin;
         }
     }
+    
+    public static void sortArrayListBasedOnScore(int[] list,Student[] students) {
+        for (int i = 0; i < list.length; i++) {
+// Find the minimum in the list[i..list.length-1]
+            int currentMin = list[i];
+            int currentMinIndex = i;
+            for (int j = i + 1; j < list.length; j++) {
+                if (currentMin > list[j]) {
+                    currentMin = list[j];
+                    currentMinIndex = j;
+                }
+            }
+// Swap list[i] with list[currentMinIndex] if necessary;if (currentMinIndex != i) {
+            list[currentMinIndex] = list[i];
+            students[currentMinIndex] = students[i];
+            list[i] = currentMin;
+        }
+    }
 
     
     public static int findMaximum(int[] array){
